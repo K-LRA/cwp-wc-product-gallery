5664c4605e055d8357a9ebf93915345a40b4cfb2
conner <38869691+K-LRA@users.noreply.github.com>
2023-11-22 09:37:25 -0600

    Update cwp-wc-product-gallery.php

--- cwp-wc-product-gallery.php
+++ cwp-wc-product-gallery.php
@@ -21,23 +21,26 @@ add_shortcode('cwp_wc_product_gallery', 'cwp_register_wc_product_gallery');
 
 function cwp_register_wc_product_gallery($atts)
 {
-    $params = array(
+
+    $default_atts = array(
         'accent_color_hex' => '#ff0000',
         'display_in_stock' => true,
         'display_oos' => true,
         'display_backorder' => true,
         'display_rating' => true,
-        'display_stock_status' => false,
+        'display_stock_status' => true,
         'display_badges' => true,
         'display_review_count' => true,
-        'display_virtual_badge' => true,
+        'display_virtual' => true,
         'display_purchase_count' => true,
         'display_emojis' => true,
     );
 
-    $params = shortcode_atts($params, $atts);
+    $atts = shortcode_atts($default_atts, $atts);
+
+    extract($atts);
 
     ob_start();
-    include plugin_dir_path(__FILE__) . 'includes/cwp-wc-product-gallery.php';
+    include plugin_dir_path(__FILE__) . 'includes/ws-wc-products.php';
     return ob_get_clean();
 }
