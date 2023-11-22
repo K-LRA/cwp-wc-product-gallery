85070b9c4af2898de079a52baf1adbf7f85fe12e
conner <38869691+K-LRA@users.noreply.github.com>
2023-11-22 09:57:11 -0600

    Update cwp-wc-product-gallery.php

--- cwp-wc-product-gallery.php
+++ cwp-wc-product-gallery.php
@@ -22,25 +22,34 @@ add_shortcode('cwp_wc_product_gallery', 'cwp_register_wc_product_gallery');
 function cwp_register_wc_product_gallery($atts)
 {
 
-    $default_atts = array(
-        'accent_color_hex' => '#ff0000',
-        'display_in_stock' => true,
-        'display_oos' => true,
-        'display_backorder' => true,
-        'display_rating' => true,
-        'display_stock_status' => true,
-        'display_badges' => true,
-        'display_review_count' => true,
-        'display_virtual' => true,
-        'display_purchase_count' => true,
-        'display_emojis' => true,
+    $atts = shortcode_atts(
+        array(
+            'accent_color_hex' => '#ff0000',
+            'display_in_stock' => true,
+            'display_oos' => true,
+            'display_backorder' => true,
+            'display_rating' => true,
+            'display_stock_status' => true,
+            'display_badges' => true,
+            'display_review_count' => true,
+            'display_virtual' => true,
+            'display_purchase_count' => true,
+            'display_emojis' => true,
+        ),
+        $atts
     );
 
-    $atts = shortcode_atts($default_atts, $atts);
+    foreach ($atts as $key => $value) {
+        if ($value === 'false' || $value === '0') {
+            $atts[$key] = false;
+        } elseif ($value === 'true' || $value === '1') {
+            $atts[$key] = true;
+        }
+    }
 
     extract($atts);
 
     ob_start();
-    include plugin_dir_path(__FILE__) . 'includes/ws-wc-products.php';
+    include plugin_dir_path(__FILE__) . 'includes/cwp-wc-product-gallery.php';
     return ob_get_clean();
 }
